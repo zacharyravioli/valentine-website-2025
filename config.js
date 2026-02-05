a123dc24d104eb6cde2a2f3fee41e1f5e16d6709
zacharyravioli <zackylee577@gmail.com>
2026-02-04 22:46:49 -0500

    Moira Valentines

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dpdekmt42/video/upload/v1770262820/Faye_Webster_-_A_Dream_With_A_Baseball_Player__mp3.pm_vceknr.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
